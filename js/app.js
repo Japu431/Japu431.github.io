7119d09ba7140dcd10e0e1e4d18d47ff3635d873
Matheus Yusuke <japamnk@gmail.com>
2022-05-03 14:34:14 -0300

    Finalizado! :D

---
+++ js/app.js
@@ -0,0 +1,165 @@
+const adicionarAluno = document.querySelector('#adicionar-aluno');
+const tabela = document.querySelector("#tabela-alunos");
+
+let buscarAlunoNoBancoDeDados = document.querySelector("#buscar-aluno");
+
+buscarAlunoNoBancoDeDados.addEventListener('click', function () {
+    for (let i = 0; i < db.length; i++) {
+        const aluno = db[i];
+        let trAluno = montaTrAluno(aluno);
+        renderAluno(trAluno)
+    }
+})
+
+
+tabela.addEventListener("dblclick", (event) => {
+
+    const askPassword = prompt('Digite a senha para remover aluno.');
+
+    if (askPassword != "12345") {
+        alert('Senha incorreta! Não foi possível remover aluno do sistema! :D')
+        return;
+    } else {
+        const alvo = event.target;
+        const alvoElementoPai = alvo.parentNode
+        alvoElementoPai.classList.add("efeito-exclui");
+        setTimeout(() => {
+            alvo.parentNode.remove();
+        }, 500)
+
+    }
+})
+
+adicionarAluno.addEventListener('click', function (e) {
+    e.preventDefault();
+    const form = document.querySelector('#form-adiciona');
+    let aluno = obtemDadosAluno(form);
+    let erros = validaCampos(aluno);
+
+    if (erros.length > 0) {
+        mostraMensagensDeErro(erros);
+        return;
+    }
+
+    let trAluno = montaTrAluno(aluno);
+    renderAluno(trAluno);
+
+    form.reset();
+
+    document.querySelector('#erros').innerHTML = '';
+
+})
+
+let filtro = document.querySelector("#filtra-aluno");
+
+filtro.addEventListener("input", () => {
+    let alunos = document.querySelectorAll(".aluno");
+
+    if (filtro.value.length > 0) {
+        for (let i = 0; i < alunos.length; i++) {
+            let aluno = alunos[i];
+
+            let tdNome = aluno.querySelector('.info-nome');
+            let nome = tdNome.textContent;
+
+            const pattern = new RegExp(filtro.value, 'i')
+
+            !pattern.test(nome) ? aluno.classList.add('invisivel') : aluno.classList.remove('invisivel')
+
+        }
+    } else {
+        for (let i = 0; i < alunos.length; i++) {
+            const aluno = alunos[i];
+            aluno.classList.remove("invisivel");
+        }
+    }
+})
+
+function verificaNotaVermelha(alunoMF) {
+    if (alunoMF < 6.0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function renderAluno(trAluno) {
+    let tabela = document.querySelector("#tabela-alunos");
+    tabela.appendChild(trAluno)
+}
+
+function montaTrAluno(aluno) {
+    console.log(aluno);
+    let tr = document.createElement('tr');
+
+    if (aluno.mediaFinal < 6.0) {
+        tr.classList.add('aluno');
+        tr.classList.add("nota-vermelha");
+    } else {
+        tr.classList.add('aluno');
+    }
+    tr.appendChild(montaTdAluno(aluno.nome, "info-nome"))
+    tr.appendChild(montaTdAluno(aluno.turma, "info-turma"))
+    tr.appendChild(montaTdAluno(aluno.nota1tri, "info-primeiro-trimestre"))
+    tr.appendChild(montaTdAluno(aluno.nota2tri, "info-segundo-trimestre"))
+    tr.appendChild(montaTdAluno(aluno.nota3tri, "info-terceiro-trimestre"))
+    tr.appendChild(montaTdAluno(aluno.mediaFinal, "info-media-final"))
+
+    return tr;
+}
+
+function montaTdAluno(content, classe) {
+    let td = document.createElement('td');
+
+    td.textContent = content;
+    td.classList.add(classe);
+
+    return td;
+}
+
+function validaCampos(aluno) {
+    const messages = [];
+
+    if (aluno.nome == '') messages.push("O campo nome não pode ser vazio!");
+    if (aluno.turma == '') messages.push("O campo turma não pode ser vazio!");
+    if (aluno.nota1tri == '') messages.push("O campo nota 1-trimestre não pode ser vazio!");
+    if (aluno.nota2tri == '') messages.push("O campo nota 2-trimestre nome não pode ser vazio!");
+    if (aluno.nota3tri == '') messages.push("O campo nota 3-trimestre nome não pode ser vazio!");
+
+    return messages;
+}
+
+
+function mostraMensagensDeErro(erros) {
+    let ul = document.querySelector('#erros');
+
+    ul.innerHTML = '';
+
+    erros.forEach(erro => {
+        let li = document.createElement('li');
+        li.classList.add('messages-erro');
+        li.textContent = erro;
+        ul.appendChild(li);
+    })
+}
+
+
+function obtemDadosAluno(form) {
+    let nome = form.querySelector("#nome");
+    let turma = form.querySelector("#turma");
+    let nota1tri = form.querySelector("#primeiro-trimestre")
+    let nota2tri = form.querySelector("#segundo-trimestre")
+    let nota3tri = form.querySelector("#terceiro-trimestre")
+
+    let aluno = {
+        nome: nome.value,
+        turma: turma.value,
+        nota1tri: nota1tri.value,
+        nota2tri: nota2tri.value,
+        nota3tri: nota3tri.value,
+        mediaFinal: ((nota1tri.value * 3 + nota2tri.value * 3 + nota3tri.value * 4) / 10).toFixed(1)
+    }
+
+    return aluno;
+}
---
+++ js/database.js
@@ -0,0 +1,50 @@
+var db = [
+    {
+        "nome": "Joana",
+        "turma": "3-Sistemas",
+        "nota1tri": 6.5,
+        "nota2tri": 5.4,
+        "nota3tri": 3.9,
+        "mediaFinal": 5.9
+    },
+    {
+        "nome": "Luiz Henrique",
+        "turma": "3-Sistemas",
+        "nota1tri": 6.5,
+        "nota2tri": 2.9,
+        "nota3tri": 10.0,
+        "mediaFinal": 7.8
+    },
+    {
+        "nome": "Jonas",
+        "turma": "3-Sistemas",
+        "nota1tri": 6.5,
+        "nota2tri": 7.9,
+        "nota3tri": 8.9,
+        "mediaFinal": 8.3
+    },
+    {
+        "nome": "Guilherme",
+        "turma": "3-Sistemas",
+        "nota1tri": 6.5,
+        "nota2tri": 10.0,
+        "nota3tri": 8.9,
+        "mediaFinal": 9.1
+    },
+    {
+        "nome": "Gabriel",
+        "turma": "3-Sistemas",
+        "nota1tri": 6.5,
+        "nota2tri": 7.9,
+        "nota3tri": 7.9,
+        "mediaFinal": 8.2
+    },
+    {
+        "nome": "Gustavo",
+        "turma": "3-Sistemas",
+        "nota1tri": 6.0,
+        "nota2tri": 7.9,
+        "nota3tri": 6.5,
+        "mediaFinal": 5.5
+    }
+]
